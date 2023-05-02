5f6ebcbb3cd65cbb83e6034114f375f419767e68
Ryo Martin <laetuzg@gmail.com>
2023-05-03 00:25:03 +0700

    Added Koin and Retrofit Dependency. Initialize Koin in MyApplication.

---
+++ app/src/main/java/id/arvigo/arvigobasecore/di/AppModules.kt
@@ -0,0 +1,32 @@
+package id.arvigo.arvigobasecore.di
+
+import id.arvigo.arvigobasecore.BuildConfig
+import id.arvigo.arvigobasecore.network.ApiService
+import id.arvigo.arvigobasecore.util.Constant.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+    single {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().apply {
+                level =
+                    if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            }
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
---
+++ app/src/main/java/id/arvigo/arvigobasecore/network/ApiService.kt
@@ -0,0 +1,4 @@
+package id.arvigo.arvigobasecore.network
+
+interface ApiService {
+}
