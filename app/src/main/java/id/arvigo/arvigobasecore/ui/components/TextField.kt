bfb1cecdacc6ee36dc4e9e43dfe691a3d70aeeb4
Ryo Martin <laetuzg@gmail.com>
2023-05-17 01:20:50 +0700

    Added component package for reusable composable items. Added TextField Class which provide TextFields Composables.

---
+++ app/src/main/java/id/arvigo/arvigobasecore/ui/components/TextField.kt
@@ -0,0 +1,87 @@
+package id.arvigo.arvigobasecore.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun UsernameTextField(
+) {
+    androidx.compose.material3.TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = null,
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        placeholder = { androidx.compose.material3.Text("Username") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .padding(horizontal = 16.dp)
+    )
+}
+
+@Composable
+fun NameTextField(
+) {
+    androidx.compose.material3.TextField(
+        value = "",
+        onValueChange = {},
+        colors = TextFieldDefaults.textFieldColors(
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        placeholder = { androidx.compose.material3.Text("Name") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .padding(horizontal = 16.dp)
+    )
+}
+
+@Composable
+fun PasswordTextField(
+) {
+    androidx.compose.material3.TextField(
+        value = "",
+        onValueChange = {},
+/*        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.,
+                contentDescription = null,
+            )
+        },*/
+        colors = TextFieldDefaults.textFieldColors(
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        placeholder = { androidx.compose.material3.Text("Password") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .padding(horizontal = 16.dp)
+    )
+}
