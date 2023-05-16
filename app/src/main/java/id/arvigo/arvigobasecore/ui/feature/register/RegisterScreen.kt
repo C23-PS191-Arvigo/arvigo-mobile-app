3a7a91ecd5f6027f7fbc04a01fc2918bb134305b
Ryo Martin <laetuzg@gmail.com>
2023-05-17 01:22:07 +0700

    Added initial UI for Register Screen.

---
+++ app/src/main/java/id/arvigo/arvigobasecore/ui/feature/register/RegisterScreen.kt
@@ -0,0 +1,69 @@
+package id.arvigo.arvigobasecore.ui.feature.register
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.arvigo.arvigobasecore.R
+import id.arvigo.arvigobasecore.ui.components.NameTextField
+import id.arvigo.arvigobasecore.ui.components.PasswordTextField
+import id.arvigo.arvigobasecore.ui.components.UsernameTextField
+import id.arvigo.arvigobasecore.ui.feature.register.ui.theme.ArvigoBaseCoreTheme
+
+@Composable
+fun RegisterScreen() {
+    RegisterScreenContent()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreenContent() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_logo),
+                contentDescription = "Arvigo Logo",
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(20.dp)
+            )
+            Text(text = "Register Your Account", style = MaterialTheme.typography.titleLarge)
+            Text(text = "Please fill the required field.", style = MaterialTheme.typography.bodyLarge.copy(color = Color.Gray))
+            Spacer(modifier = Modifier.padding(16.dp))
+            NameTextField()
+            Spacer(modifier = Modifier.padding(16.dp))
+            UsernameTextField()
+            Spacer(modifier = Modifier.padding(16.dp))
+            PasswordTextField()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    ArvigoBaseCoreTheme {
+        RegisterScreen()
+    }
+}
