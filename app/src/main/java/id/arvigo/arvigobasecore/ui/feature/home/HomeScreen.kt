f3078e1d0dd05d168414d5ce4da72cdc94fb4285
Irfan Maulana <irfan.resimen2018@gmail.com>
2023-05-17 15:44:37 +0700

    feat: add app bar with primary searchbar

--- app/src/main/java/id/arvigo/arvigobasecore/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/id/arvigo/arvigobasecore/ui/feature/home/HomeScreen.kt
@@ -1,9 +1,62 @@
 package id.arvigo.arvigobasecore.ui.feature
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.arvigo.arvigobasecore.ui.component.PrimarySearch
 
 @Composable
 fun HomeScreen() {
-    Text(text = "home")
+   HomeContent()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeContent() {
+    val text by remember { mutableStateOf("") }
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            SmallTopAppBar(
+                title = {
+                    PrimarySearch(query = text)
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Notifications,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier.padding(it)
+        ) {
+            
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomePrev() {
+
 }
