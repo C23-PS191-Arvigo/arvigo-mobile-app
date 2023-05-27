2ff55c362ea1440b91b1570a702bce1c63d077a4
Irfan Maulana <irfan.resimen2018@gmail.com>
2023-05-27 07:53:34 +0700

    feat: adding personality test screen

--- app/src/main/java/id/arvigo/arvigobasecore/ui/feature/personality/PersonalityMainTestScreen.kt
+++ app/src/main/java/id/arvigo/arvigobasecore/ui/feature/personality/PersonalityMainTestScreen.kt
@@ -1,11 +1,16 @@
 package id.arvigo.arvigobasecore.ui.feature.personality
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.arvigo.arvigobasecore.ui.component.PrimaryButton
 
 @Composable
 fun PersonalityMainTestScreen() {
@@ -22,8 +27,83 @@ fun PersonalityMainTestContent() {
             modifier = Modifier
                 .padding(it)
         ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(3.8f)
+            ) {
+                items(10) {
+                    PersonalTestCard()
+                }
+            }
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.3f)
+                    .padding(horizontal = 15.dp, vertical = 4.dp)
+            ) {
+                PrimaryButton(title = "Continue", onClick = {
 
+                } )
+            }
         }
     }
 
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PersonalTestCard() {
+
+    var state by remember {
+        mutableStateOf(false)
+    }
+
+    val radioOptions = listOf("1", "2", "3", "4", "5")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[2] ) }
+
+    Surface() {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Contoh soal test kepribadian dari API", style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.padding(top = 12.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(text = "Strongly\nDisagree", style = MaterialTheme.typography.titleMedium)
+                radioOptions.forEach { text ->
+                    Box(
+                        Modifier
+                            .selectable(
+                                selected = (text == selectedOption),
+                                onClick = {
+                                    onOptionSelected(text)
+                                }
+                            )
+                    ) {
+                        RadioButton(
+                            selected = (text == selectedOption),
+                            onClick = { onOptionSelected(text) }
+                        )
+                    }
+                }
+                Text(text = "Strongly\nAgree", style = MaterialTheme.typography.titleMedium)
+            }
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+            Divider()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PersonalTestCardPrev() {
+    PersonalTestCard()
 }
